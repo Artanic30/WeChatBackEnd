4e4ab6b74e9d32e63bc06007fcef8c436899db34
Artanic <599253487@qq.com>
2020-07-15 19:55:27 +0800

    update add unique restrain

--- Backend/Orchestral/models.py
+++ Backend/Orchestral/models.py
@@ -26,20 +26,20 @@ class Absence(models.Model):
 
 
 class NameList(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
 
 
 class WindNameList(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
 
 
 class StringedNameList(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
 
 
 class PercussionNameList(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
 
 
 class NotificationEmailList(models.Model):
-    email = models.EmailField(max_length=50)
+    email = models.EmailField(max_length=50, unique=True)

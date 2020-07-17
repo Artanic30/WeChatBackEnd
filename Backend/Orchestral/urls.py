ec992d8d0a9b090a66c1f4546538aa9433654c6d
Artanic <599253487@qq.com>
2020-07-17 13:46:50 +0800

    update fix django 2.x -> 3.x

--- Backend/Orchestral/urls.py
+++ Backend/Orchestral/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 router = SimpleRouter()
 
-router.register(r'absence', views.AbsenceViewSet, base_name='absence')
-router.register(r'manager', views.ManagerViewSet, base_name='manager')
-router.register(r'account', views.AccountsViewSet, base_name='account')
+router.register(r'absence', views.AbsenceViewSet, basename='absence')
+router.register(r'manager', views.ManagerViewSet, basename='manager')
+router.register(r'account', views.AccountsViewSet, basename='account')
 
 
 urlpatterns = router.urls
